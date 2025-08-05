f9fd10cc9d730e2fc0c5440b291df963ec19f5af
Nabira Rashid <nabira.rashidm@gmail.com>
2025-08-05 18:03:39 -0400

    removed heart

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
-import { Search, Heart, User, Settings } from "lucide-react";
+import { Search, User, Settings } from "lucide-react";
 import CustomerModal from "./CustomerModal";
 import AdminModal from "./Admin";
 
