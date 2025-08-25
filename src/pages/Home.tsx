76004365892962f39c778126e4d0e3d48aed892c
Nabira Rashid <nabira.rashidm@gmail.com>
2025-08-25 17:03:08 -0400

    randomizer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { db } from "../firebase";
-import { collection, getDocs, limit, query } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
 import { PLACEHOLDER_IMAGES } from "../utils/placeholders";
 
 const Home = () => {
